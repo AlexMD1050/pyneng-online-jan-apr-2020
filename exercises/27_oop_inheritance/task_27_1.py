9b56979b7df566cd3c96dc5430e6745b2d6869e4
AlexMD1050 <alexMD@gmail.com>
2020-04-06 18:25:38 +0300

    06.04.2020 did tasks 27_1, 27_2, 27_2a

--- exercises/27_oop_inheritance/task_27_1.py
+++ exercises/27_oop_inheritance/task_27_1.py
@@ -27,26 +27,6 @@ import netmiko
 import os
 from base_connect_class import BaseSSH
 
-'''
-class BaseSSH:
-    def __init__(self, **device_params):
-        self.ssh = netmiko.ConnectHandler(**device_params)
-
-    def send_show_command(self, command):
-        return self.ssh.send_command(command)
-
-    def send_cfg_commands(self, commands):
-        return self.ssh.send_config_set(commands)
-'''
-class BaseSSH:
-    def __init__(self, **device_params):
-        self.ssh = netmiko.ConnectHandler(**device_params)
-
-    def send_show_command(self, command):
-        return self.ssh.send_command(command)
-
-    def send_cfg_commands(self, commands):
-        return self.ssh.send_config_set(commands)
 
 class CiscoSSH(BaseSSH):
     def __init__(self, **kwargs):
